9d9fec120e7bed920bbda74fb911d268a71b90ba
Tatyana Evtikhova <tatyanaevtihova@gmail.com>
2025-03-12 18:23:15 +0300

    Lesson 1 added

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function startLesson() {
     alert("Урок начинается!");
     // Логика для начала урока
 }
+
+function speak(text) {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'zh-CN';
+    speechSynthesis.speak(utterance);
+}
